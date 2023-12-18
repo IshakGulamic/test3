c0fd61340bc01330979b33a868cbdd2cbb56945b
IshakGulamic <153982793+IshakGulamic@users.noreply.github.com>
2023-12-18 14:53:19 +0100

    Update test_python.py

--- test_python.py
+++ test_python.py
@@ -1,8 +1,8 @@
 someNumber = 10
 
-if(someNumber >= 0):
-    print("Over zero")
+if(someNumber > 0):
+    print("Over zerro")
 elif(someNumber < 0):
-    print("Bellow zero")
+    print("Bellow zerro")
 else:
-    print("zero")
+    print("zerro")
